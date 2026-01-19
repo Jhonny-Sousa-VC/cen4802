bd6483e8e3ab17c9e39f84c041f3ce77b7dc2bc9
Jhonny-Sousa-VC <jsousadossantos@mail.valenciacollege.edu>
2026-01-18 21:57:21 -0400

    Add Javadoc to fibonacci method

--- FibonacciApp.java
+++ FibonacciApp.java
@@ -1,8 +1,16 @@
 public class FibonacciApp {
 
+    /**
+     * Computes the nth term of the Fibonacci sequence using recursion.
+     * The Fibonacci sequence is defined as F(0)=0, F(1)=1, and
+     * F(n)=F(n-1)+F(n-2) for n > 1.
+     *
+     * @param n the position in the Fibonacci sequence (0 or greater)
+     * @return the nth Fibonacci term
+     */
     public static int fibonacci(int n) {
         if (n <= 1) {
-            return n; // F(0)=0, F(1)=1
+            return n;
         }
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
